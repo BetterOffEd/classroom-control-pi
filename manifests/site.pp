6d2df0f9402e7d936c931072c776e84711c9a250
gitgtt <gtt@pdx.edu>
2016-06-01 14:02:49 -0700

    Update site.pp
    commiting

--- manifests/site.pp
+++ manifests/site.pp
@@ -22,8 +22,6 @@
 # specified in the console for that node.
 
 node default {
-  # This is where you can declare classes for all nodes.
-  # Example:
-  #   class { 'my_class': }
-  notify { "This is the default message from the production environment": }
+#...
+notify { "Hello world! I am ${::fqdn}": }
 }
