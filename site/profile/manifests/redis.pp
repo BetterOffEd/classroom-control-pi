013b88ebaa4b3d639a91c97667ce12a8b0dcb411
gitgtt <gtt@pdx.edu>
2016-06-02 15:48:29 -0700

    Update redis.pp

--- site/profile/manifests/redis.pp
+++ site/profile/manifests/redis.pp
@@ -4,12 +4,12 @@ $master = false,
 include profile::epel
 if $master {
 class {'redis':
-maxmemory => 10mb,
+maxmemory => '10mb',
 bind => $ipaddress,
 }
 } else {
 class {'redis':
-maxmemory => 10mb,
+maxmemory => '10mb',
 bind => $ipaddress,
 slaveof => 'master.puppetlabs.vm 6479',
 }
