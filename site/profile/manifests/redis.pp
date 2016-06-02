4d1667d7d55c38a132b80fc06dacab4a677fe026
Zack Smith <acidprime@wallcity.org>
2016-06-02 15:38:00 -0700

    Update redis.pp

--- site/profile/manifests/redis.pp
+++ site/profile/manifests/redis.pp
@@ -1,2 +1,16 @@
-class profile::redis {
+class profile::redis (
+  $master = false,
+) {
+if $master {
+class {'redis':
+maxmemory => 10mb,
+bind => $ipaddress,
+}
+} else {
+class {'redis':
+maxmemory => 10mb,
+bind => $ipaddress,
+slaveof => 'master.puppetlabs.vm 6479',
+}
+}
 }
