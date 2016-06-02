c189e790f3ca1aa36ee948e7eef3b8712257bbce
moorejpdx <james.moore.pdx@gmail.com>
2016-06-02 15:41:47 -0700

    added class profile

--- site/profile/manifests/redis.pp
+++ site/profile/manifests/redis.pp
@@ -1,12 +1,17 @@
-if $master { 
-  class { 'redis':
-          maxmemory => '10mb',
-          bind => $ipaddress, 
-        }
-} else {
-  class { 'redis':
-          maxmemory => '10mb',
-          bind     => $ipaddress,
-          slaveof => 'master.puppetlabs.vm 6479',
-        } 
+class profile::redis ( $master = false,
+){
+  include profile::epel
+  
+  if $master { 
+    class { 'redis':
+            maxmemory => '10mb',
+            bind => $ipaddress, 
+          }
+  } else {
+    class { 'redis':
+            maxmemory => '10mb',
+            bind     => $ipaddress,
+            slaveof => 'master.puppetlabs.vm 6479',
+          } 
+  }
 }
