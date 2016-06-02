de736d02023385ce21ec10ce690553ea506c7767
moorejpdx <james.moore.pdx@gmail.com>
2016-06-02 15:35:29 -0700

    redis quotes...yay

--- site/profile/manifests/redis.pp
+++ site/profile/manifests/redis.pp
@@ -1,11 +1,11 @@
 if $master { 
   class { 'redis':
-          maxmemory => 10mb,
+          maxmemory => '10mb',
           bind => $ipaddress, 
         }
 } else {
   class { 'redis':
-          maxmemory => 10mb,
+          maxmemory => '10mb',
           bind     => $ipaddress,
           slaveof => 'master.puppetlabs.vm 6479',
         } 
