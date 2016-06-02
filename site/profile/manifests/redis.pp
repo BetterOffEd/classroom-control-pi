afb4f05f1b12dcf442b3c30ec4f17733b480153c
brlehigh <brlehigh@pdx.edu>
2016-06-02 15:44:23 -0700

    Update redis.pp

--- site/profile/manifests/redis.pp
+++ site/profile/manifests/redis.pp
@@ -6,12 +6,12 @@ include profile::epel
 
 if $master {
   class {'redis':
-    maxmemory => 10mb,
+    maxmemory => '10mb',
     bind => $ipaddress,
   }
 } else {
   class {'redis':
-    maxmemory => 10mb,
+    maxmemory => '10mb',
     bind => $ipaddress,
     slaveof => 'master.puppetlabs.vm 6479',
     }
