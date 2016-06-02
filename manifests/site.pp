48783edecfd4ff65268c76462727227aa6b14abe
root <root@tamgc.puppetlabs.vm>
2016-06-02 03:48:01 +0000

    Excercise 4.1

--- manifests/site.pp
+++ manifests/site.pp
@@ -25,5 +25,6 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  notify { "This is the default message from the production environment": }
+  # notify { "This is the default message from the production environment": }
+  notify { "Hello world! I am ${:fqdn{": }
 }
