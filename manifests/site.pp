6573794edf0ccde83cd05e227e89b6944137f215
tamgc <tamgc@tamgc.net>
2016-06-02 11:06:44 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -23,5 +23,7 @@
 
 node default {
 #...
-notify { "Hello world! I am ${::fqdn}": }
+  unless $environment in ['production,'staging']{
+    notify { "Warning: this is a development environment on ${::fqdn}": }
+  }
 }
