5ac6eca3b134eddca72300498a079f0e164bd782
Sean Ellefson <scellef@gmail.com>
2016-06-03 11:40:40 -0700

    Exercise 13.1

--- site/aliases/manifests/init.pp
+++ site/aliases/manifests/init.pp
@@ -8,7 +8,7 @@ class aliases (
         owner   => 'root',
         group   => 'root',
         mode    => '0644',
-        content => ???,
+        content => epp('aliases.pp.epp', { admin => $admin }),
     }
     exec { '/usr/bin/newaliases':
         refreshonly => true,
