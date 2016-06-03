95a49293df157a3aa133e16ac0fabbf28070ecfc
BetterOffEd <ecarpent@gmail.com>
2016-06-03 14:11:07 -0700

    lab 15.2

--- site/dsciis/manifests/init.pp
+++ site/dsciis/manifests/init.pp
@@ -1,8 +1,8 @@
 class dsciis {
 
-  # TODO: add the correct name to this resource so DSC can trigger reboots
-  reboot { ???:
-    when    => pending,
+  # Corrected name to this resource so DSC can trigger reboots
+  reboot { 'dsc_reboot':
+    when => pending,
     timeout => 15,
   }
 
@@ -15,10 +15,47 @@ class dsciis {
   vcsrepo { 'C:/inetpub/puppetize':
     ensure   => present,
     provider => git,
-    source   => 'https://github.com/puppetlabs-education/asp-starter-site.git',
+    source => 'https://github.com/puppetlabs-education/classroom-control-pi.git',
+    revision => 'production',
   }
 
-  # Translate the provided DSC Powershell script into Puppet code below
-
-  # don't forget to add the appropriate relationships
+  # Translated the provided DSC Powershell script into Puppet code below
+  dsc_windowsfeature{'iis':
+    dsc_ensure => 'Present',
+    dsc_name => 'Web-Server',
+    }
+  -> dsc_windowsfeature{'iisscriptingtools':
+    dsc_ensure => 'Present',
+    dsc_name => 'Web-Scripting-Tools',
+    }
+  -> dsc_windowsfeature{'aspnet45':
+    dsc_ensure => 'Present',
+    dsc_name => 'Web-Asp-Net45',
+    }
+  -> dsc_xwebsite{'defaultsite':
+    dsc_ensure => 'Present',
+    dsc_name => 'Default Web Site',
+    dsc_state => 'Stopped',
+    dsc_physicalpath => 'C:\inetpub\wwwroot',
+    }
+  -> dsc_xwebapppool{'newwebapppool':
+    dsc_name => 'PuppetCodezAppPool',
+    dsc_ensure => 'Present',
+    dsc_state => 'Started',
+    }
+  -> dsc_xwebsite{'newwebsite':
+    dsc_ensure => 'Present',
+    dsc_name => 'PuppetCodez',
+    dsc_state => 'Started',
+    dsc_physicalpath => 'C:\inetpub\puppetize',
+    dsc_applicationpool => 'PuppetCodezAppPool',
+    dsc_bindinginfo => [{
+      protocol => 'HTTP',
+      port => 80,
+      }],
+    require => [
+      Vcsrepo['C:/inetpub/puppetize'], # added dependencies
+      Package['dotnet4.5.2'],
+      ],
+    }
 }
