399de6688598ced837a64b69c19384b577c688bf
moorejpdx <james.moore.pdx@gmail.com>
2016-06-03 09:56:36 -0700

    window tuning stuff

---
+++ site/profile/tuning/windows.pp
@@ -0,0 +1,16 @@
+class profile::tuning::windows { 
+  registry::value { 'KeepAliveInterval':
+                    key => 'HKLM\SYSTEM\CurrentControlSet\Services\AFD\Parameters', value => 'KeepAliveInterval',
+                    data => '1',
+  }
+class profile::tuning::windows { 
+  registry::value { 'KeepAliveInterval':
+                    key => 'HKLM\SYSTEM\CurrentControlSet\Services\TCPIP\Parameters', value => 'TcpMaxDataRetransmissions',
+                    data => '5',
+  }
+class profile::tuning::windows { 
+  registry::value { 'KeepAliveInterval':
+                    key => 'HKLM\SYSTEM\CurrentControlSet\Services\TCPIP\Parameters', value => 'TcpTimedWaitDelay',
+                    data => '0x0000001e',
+  }
+  
