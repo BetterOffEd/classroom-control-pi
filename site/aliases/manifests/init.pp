fd6480f0c437cfa6d42f414fc85fdb4ce1785d3e
BetterOffEd <ecarpent@gmail.com>
2016-06-03 11:37:02 -0700

    lab 13.1

--- site/aliases/manifests/init.pp
+++ site/aliases/manifests/init.pp
@@ -2,16 +2,17 @@ class aliases (
     $admin = 'root',
 ) {
     # uses $admin to build the aliases file
-    # TODO: Add the function call to generate text from a template
     file { '/etc/aliases':
-        ensure  => file,
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0644',
-        content => ???,
+        ensure => file,
+        owner => 'root',
+        group => 'root',
+        mode => '0644',
+        content => epp('aliases/aliases.epp', {
+            admin => $admin,
+        }),
     }
     exec { '/usr/bin/newaliases':
         refreshonly => true,
-        subscribe   => File['/etc/aliases'],
+        subscribe => File['/etc/aliases'],
     }
 }
