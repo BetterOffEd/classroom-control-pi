6dbb4d6719dd671e43562a11ecc694377e2480ae
Sean Ellefson <scellef@gmail.com>
2016-06-03 10:05:05 -0700

    Exercise 12.4

--- site/skeleton/manifests/managed_user.pp
+++ site/skeleton/manifests/managed_user.pp
@@ -1,6 +1,7 @@
 # TODO: Add a password parameter
 define skeleton::managed_user (
   $home = undef,
+  $password = hunter2,
 ) {
   if $home {
     $homedir = $home
@@ -42,6 +43,10 @@ define skeleton::managed_user (
     }
 
     # TODO: Add a file resource to manage "${homedir}/.bashrc"
+    file { "${homedir}/.bashrc":
+      ensure => file,
+      source => 'puppet:///modules/skeleton/profile.ps1',
+    }
 
   }
 
@@ -52,6 +57,7 @@ define skeleton::managed_user (
   user { $name:
     ensure     => present,
     managehome => true,
+    password   => $password,
   }
 
   file { $homedir:
