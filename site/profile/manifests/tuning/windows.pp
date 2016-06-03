f7ffb8b206da4bb6471e1b0fc084b061bb3af852
moorejpdx <james.moore.pdx@gmail.com>
2016-06-03 10:07:30 -0700

    stupid mistake

--- site/profile/manifests/tuning/windows.pp
+++ site/profile/manifests/tuning/windows.pp
@@ -3,12 +3,10 @@ class profile::tuning::windows {
                     key => 'HKLM\SYSTEM\CurrentControlSet\Services\AFD\Parameters', value => 'KeepAliveInterval',
                     data => '1',
   }
-class profile::tuning::windows { 
   registry::value { 'KeepAliveInterval':
                     key => 'HKLM\SYSTEM\CurrentControlSet\Services\TCPIP\Parameters', value => 'TcpMaxDataRetransmissions',
                     data => '5',
   }
-class profile::tuning::windows { 
   registry::value { 'KeepAliveInterval':
                     key => 'HKLM\SYSTEM\CurrentControlSet\Services\TCPIP\Parameters', value => 'TcpTimedWaitDelay',
                     data => '0x0000001e',
