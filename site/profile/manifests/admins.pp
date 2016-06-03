0b1f59810cf7bfd3dcf7374ed1d4b9bf018e01a7
BetterOffEd <ecarpent@gmail.com>
2016-06-03 11:05:49 -0700

    12.4

--- site/profile/manifests/admins.pp
+++ site/profile/manifests/admins.pp
@@ -2,10 +2,9 @@ class profile::admins {
   # Windows requires a plain text password, but Linux requires a hash.
   # Both password values are the same.
   $password = $osfamily ? {
-    'RedHat'  => '$1$HdDw//gC$2VBiQ1x5blLPwNS.G.Iw21',
+    'RedHat' => '$1$HdDw//gC$2VBiQ1x5blLPwNS.G.Iw21',
     'windows' => 'Puppet8Labs!',
   }
-
   skeleton::managed_user { ['jose', 'alice', 'bob']:
     password => $password,
   }
